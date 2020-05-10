274c7c28003d5353878b7d4ce2c70bb949ab0e53
Aris Cruz <ariscruz0117@gmail.com>
2020-05-10 11:37:59 -0500

    ready to separate

---
+++ scripts/login.js
@@ -0,0 +1,48 @@
+let users = [];
+
+let registerBtn = document.getElementById("registerBtn");
+registerBtn.addEventListener("click", register);
+
+function register(event) {
+
+    let firstname = ''
+    let lastname = ''
+    let trivia = []
+    let username = document.getElementById("username").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    let password_check = document.getElementById("password2").value;
+    let password_input = document.getElementById("password2");
+
+    if(password != password_check) {
+        password_input.setCustomValidity("Las contraseñas no son iguales.");
+        return;
+    }
+    
+    
+    event.preventDefault();
+
+    let str = JSON.stringify({
+            
+            //'firstname': firstname,
+            //'lastname': lastname,
+            "username": username,
+            "email": email,
+            "password": password
+
+    });
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', '/api/user');
+    xhr.setRequestHeader('content-type','application/json');
+    xhr.send(str);
+    xhr.onload = function(){
+        if(xhr.status != 201){
+            alert(xhr.status+ ': '+ xhr.statusText + "\n Un error ha ocurrido.");
+        }else{
+            alert(xhr.status+ ': '+ xhr.statusText + "\n Creado.");
+            location.href = 'jugando.html';
+        }
+    }
+
+}
